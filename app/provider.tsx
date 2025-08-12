079ba547f58f58b08a9d607fd110b9b89a3a5f88
Manoj Sharma <148334299+manoj1234-ms@users.noreply.github.com>
2025-08-12 12:59:54 +0530

    Arcject setup complete

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Header from "./components/Header";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useUser } from "@clerk/nextjs";
+import { userDetailContext } from "@/context/userDetailContext";
 
 function Provider({
   children,
@@ -14,26 +15,37 @@ function Provider({
   const CreateUser = useMutation(api.user.CreateNewUser);
   const { user } = useUser();
 
+  const [userDetail, setUserDetail] = useState<any>();
+
   useEffect(() => {
     user && CreateNewUser();
   }, [user]);
 
   const CreateNewUser = async () => {
     if (user) {
-    }
+    
     // Save New User if not exist
     const result = await CreateUser({
       email: user?.primaryEmailAddress?.emailAddress ?? "",
       imageUrl: user?.imageUrl,
       name: user?.fullName ?? "",
     });
+    setUserDetail(result);
+  }
+
   };
   return (
+    <userDetailContext.Provider value={{userDetail,setUserDetail}}>
     <div>
       <Header />
       {children}
     </div>
+    </userDetailContext.Provider>
   );
 }
 
 export default Provider;
+
+export const useUserDetail = () => {
+  return useContext(userDetailContext);
+}
